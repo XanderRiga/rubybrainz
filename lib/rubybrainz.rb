71de6ce268d5688ec4a1d45abd49ed59b424f88b
Xander <xanderriga@live.com>
2020-11-25 20:13:32 -0800

    Require artist parameter in the rubybrainz module

--- lib/rubybrainz.rb
+++ lib/rubybrainz.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rubybrainz/version'
+require 'rubybrainz/entities/request/artist_parameter'
 
 # This is the base module to use all of the rubybrainz module.
 module Rubybrainz
